1235df991d2222fe2a4feadb80c0d77637318f9a
tengattack <tengattack@foxmail.com>
2017-04-04 22:02:38 +0800

    Add research tab

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,21 @@
 
 const gulp = require('gulp')
 const ejs = require('gulp-ejs')
+const removeHtmlComments = require('gulp-remove-html-comments')
+const htmlBeautify = require('gulp-html-beautify')
+
+const beautifyOpts = {
+  indent_size: 2,
+  preserve_newlines: false,
+  space_after_anon_function: true,
+  extra_liners: [],
+}
 
 gulp.task('html', function () {
   return gulp.src('views/*.ejs')
-    .pipe(ejs({}, {}, { ext: '.html' }))
+    .pipe(ejs({}, { /*rmWhitespace: true*/ }, { ext: '.html' }))
+    .pipe(removeHtmlComments())
+    .pipe(htmlBeautify(beautifyOpts))
     .pipe(gulp.dest('.'))
 })
 
